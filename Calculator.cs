06dbf7eb9dde4e24b07b1e064e77d9e67f3d7b13
nakesh. <77782202+nak3sh@users.noreply.github.com>
2025-01-08 22:00:10 +0100

    publish

---
+++ Calculator.cs
@@ -0,0 +1,57 @@
+﻿// https://github.com/voldiriddle 
+// https://github.com/nak3sh
+
+using System;
+
+
+class felix
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Taschenrechner ");
+
+        Console.WriteLine("Erste Zahl bitte: \n");
+        string Zahl1 = Console.ReadLine(); ;
+        int Zahl11 = Convert.ToInt32(Zahl1);    
+        //Console.WriteLine(Zahl1);
+
+        Console.WriteLine("Zweite Zahl bitte: \n");
+        string Zahl2 = Console.ReadLine(); ;
+        int Zahl22 = Convert.ToInt32(Zahl2);
+        //Console.WriteLine(Zahl2);
+
+        Console.WriteLine($"Erste Zahl: {Zahl1} Zweite Zahl: {Zahl2}");
+
+        Console.WriteLine("Rechensysmbol bitte: (+/-/*//)");
+        string symbol = Console.ReadLine();
+        //Console.WriteLine(symbol);
+        int result;
+        switch (symbol)
+        {
+            case "+":
+                result = Zahl11 + Zahl22;
+                break;
+            case "-":
+                result = Zahl11 - Zahl22;
+                break;
+            case "*":
+                result = Zahl11 * Zahl22;
+                break;
+            case "/":
+                if (Zahl22 == 0) { 
+                    Console.WriteLine("Man kann nicht durch 0 teilen!");
+                    return; }
+                else
+                    result = Zahl11 / Zahl22;
+                break;
+
+            default:
+                result = 0;
+                break;
+
+        }
+        
+        Console.WriteLine($"Das Ergebniss ist: {result}");
+
+    }
+}
